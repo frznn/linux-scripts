5ba10470893d088b4f2272a606de1147a66aa6c3
frznn <frznn.it@zohomail.eu>
2022-06-20 16:18:05 +0200

    add setup script

---
+++ setup/ubuntu22-setup.sh
@@ -0,0 +1,68 @@
+
+sudo apt update -y
+sudo apt upgrade -y
+
+# Enable AppImages
+sudo apt-get install fuse libfuse2
+# Install AppImageLauncher
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:appimagelauncher-team/stable
+sudo apt update
+sudo apt install appimagelauncher
+
+# Remove Snap
+sudo apt-get autopurge snapd
+
+cat <<EOF | sudo tee /etc/apt/preferences.d/nosnap.pref
+# To prevent repository packages from triggering the installation of Snap,
+# this file forbids snapd from being installed by APT.
+# For more information: https://linuxmint-user-guide.readthedocs.io/en/latest/snap.html
+
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOF
+
+
+# Download Bitwarden
+wget 'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=appimage' -O Bitwarden.AppImage
+chmod +x Bitwarden.AppImage
+./Bitwarden.AppImage &
+
+
+# Install Firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
+sudo apt install firefox
+
+
+# Install Brave
+sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+sudo apt install brave-browser
+
+# Install Spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+
+# Work
+sudo apt install git
+cd Downloads
+git clone https://github.com/scheib/chromium-latest-linux
+cd chromium-latest-linux
+source update-and-run.sh
