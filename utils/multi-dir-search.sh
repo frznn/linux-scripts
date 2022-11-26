611c59a45cca1e17a9790c7793d575d677740379
Federico Franzoni <8609060+fed-franz@users.noreply.github.com>
2022-11-26 13:13:29 +0100

    search multiple words

--- utils/multi-dir-search.sh
+++ utils/multi-dir-search.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-NAME=$1
+WORD1=$1
+WORD2=$2
+WORD3=$3
 
-DIR1="/media/frznn/Movie DB"
-DIR2="/media/frznn/Video/Film"
-DIR3="/media/frznn/Download/MegaDownload"
+DIR1="/etc/"
+DIR2="/media/"
+DIR3="/usr/"
 
-find "$DIR1" "$DIR2" "$DIR3" -iname "*$NAME*"
+find "$DIR1" "$DIR2" "$DIR3" -iname "*$WORD1*$WORD2*$WORD3"
+
