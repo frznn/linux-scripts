5082cbbfbc5bfa0c2e1a6d95c3798dafa3e08247
Federico Franzoni <8609060+fed-franz@users.noreply.github.com>
2022-12-11 01:51:26 +0100

    split results in columns

--- utils/multi-dir-search.sh
+++ utils/multi-dir-search.sh
@@ -8,5 +8,16 @@ DIR1="/etc/"
 DIR2="/media/"
 DIR3="/usr/"
 
-find "$DIR1" "$DIR2" "$DIR3" -iname "*$WORD1*$WORD2*$WORD3"
+printFormat="%-100s %-30s\n"
+printf "${printFormat}" "File" "Path"
+printf "${printFormat}" "========================================" "========================================"
 
+# Find matching files and print in columns
+find "$DIR1" "$DIR2" "$DIR3" -iname "*$WORD1*$WORD2*$WORD3" | \
+while read filename
+do
+    path=`dirname "$filename"`
+    file=`basename "$filename"`
+
+    printf "${printFormat}" "$file" "$path"
+done
