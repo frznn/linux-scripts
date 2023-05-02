b3f456f98112ba6248310e470214d745fb4214d0
Federico Franzoni <8609060+fed-franz@users.noreply.github.com>
2023-05-02 16:04:40 +0200

    Move Snap cleanup to clean-sw

--- cleanup/clean-sw.sh
+++ cleanup/clean-sw.sh
@@ -1,2 +1,12 @@
 #NPM
 npm cache clean --force
+
+#Snap
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
