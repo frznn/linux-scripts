1d7c73f998dfeb30c85ac7d2a09f208896fc6543
Federico Franzoni <8609060+fed-franz@users.noreply.github.com>
2023-07-05 12:48:03 +0200

    cleanup

--- utils/github/getNewRepos.sh
+++ utils/github/getNewRepos.sh
@@ -13,21 +13,17 @@ NUM_REPOS="$3"
 
 URL="https://api.github.com/orgs/$ORG/repos?sort=created_at\&per_page=$NUM_REPOS"
 
-H="-H \"Accept: application/vnd.github+json\" \
-  -H \"Authorization: Bearer $TOKEN\"\ 
-  -H \"X-GitHub-Api-Version: 2022-11-28\""
+# H="-H \"Accept: application/vnd.github+json\" \
+#   -H \"Authorization: Bearer $TOKEN\"\ 
+#   -H \"X-GitHub-Api-Version: 2022-11-28\""
 
-# response=$(curl -s -L $H $URL)
-
-H="-H \"Accept: application/vnd.github+json\"   -H \"Authorization: Bearer $TOKEN\"  -H \"X-GitHub-Api-Version: 2022-11-28\" "
 H1="-H \"Accept: application/vnd.github+json\""
 # Using H2 doesn't work, probably due to $TOKEN not being replaced correctly in the string
 #H2="-H \"Authorization: Bearer $TOKEN\""
-
 H3="-H "X-GitHub-Api-Version: 2022-11-28""
 
 response=$(curl -s -L $H1 -H "Authorization: Bearer $TOKEN" $H3 $URL)
-# echo $response | jq '.[].name'
+
 echo $response | jq '.[].name'
 
 
