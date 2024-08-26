0a1e8c00fdd285e94e46e33ec4c20a91b744227c
Federico Franzoni <8609060+fed-franz@users.noreply.github.com>
2024-08-26 10:53:44 +0200

    fix sudo find logs

--- cleanup/clean.sh
+++ cleanup/clean.sh
@@ -17,40 +17,49 @@ remove_old_kernels () {
   yes | apt purge  $OLD_KERNELS
 }
 
+sudo remove_old_kernels
 
-remove_old_kernels
+echo "Old Kernels Removed ($?)"
 
 
 #Check the Drive Space Used by Cached Files
-du -sh /var/cache/apt/archives
+sudo du -sh /var/cache/apt/archives
 
 #Delete cache file older than 30 days
-find ~/.cache/ -type f -atime +30 -delete
+echo "Delete old cache files"
+sudo find ~/.cache/ -type f -atime +30 -delete
 
 #Clean all the log file
 #for logs in `find /var/log -type f`;  do > $logs; done
-
-logs=`find /var/log -type f`
+echo "Clean log files"
+logs=`sudo find /var/log -type f`
 for i in $logs
 do
-sudo rm $i
+sudo rm -f $i
 done
 
-#Getting rid of partial packages
-sudo apt clean && apt autoclean
-# sudo apt autoremove   ## this removes too much stuff (e.g. build-essential)
+#Delete all .gz and rotated file
+sudo find /var/log -type f -regex ".*\.gz$" | xargs sudo rm -Rf
+sudo find /var/log -type f -regex ".*\.[0-9]$" | xargs sudo rm -Rf
+
 
 #Getting rid of orphaned packages
 #TODO: check if deborphan is installed
+echo "Clean orphan packages"
 deborphan | xargs sudo apt-get -y remove --purge
 
 #Free up space by clean out the cached packages
-sudo apt-get clean
+#Getting rid of partial packages
+echo "Clean APT"
+sudo apt clean && sudo apt autoclean
+# sudo apt autoremove   ## this removes too much stuff (e.g. build-essential)
 
 # Remove old logs
+echo "Remove old journal logs"
 sudo journalctl --vacuum-time=7days
 
 # Remove the Trash
+echo "Empty trash"
 rm -rf /home/*/.local/share/Trash/*/**
 sudo rm -rf /root/.local/share/Trash/*/**
 
@@ -58,9 +67,6 @@ sudo rm -rf /root/.local/share/Trash/*/**
 #rm -rf /usr/share/man/?? 
 #rm -rf /usr/share/man/??_*
 
-#Delete all .gz and rotated file
-sudo find /var/log -type f -regex ".*\.gz$" | xargs sudo rm -Rf
-sudo find /var/log -type f -regex ".*\.[0-9]$" | xargs sudo rm -Rf
 
 #Cleaning the old kernels
 #dpkg-query -l|grep linux-im*
@@ -69,6 +75,7 @@ sudo find /var/log -type f -regex ".*\.[0-9]$" | xargs sudo rm -Rf
 #sudo apt-get install linux-headers-`uname -r|cut -d'-' -f3`-`uname -r|cut -d'-' -f4`
 
 # Clean cache folder
+echo "Clean cache folder"
 rm -rf $HOME/.cache/*
 
 #Cleaning is completed
